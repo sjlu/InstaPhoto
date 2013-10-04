cc78513bcd0cda4c87f5a9cd4ccdd091a3392b63
Steven Lu <tacticalazn@gmail.com>
2013-10-03 23:38:23 -0400

    Scrollable view.

--- InstaPhoto/ProfileViewController.m
+++ InstaPhoto/ProfileViewController.m
@@ -27,7 +27,35 @@
 - (void)viewDidLoad
 {
   [super viewDidLoad];
+  self.scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+  self.scrollView.autoresizingMask = UIViewAutoresizingFlexibleHeight;
+  self.scrollView.contentSize = CGSizeMake(320, 600);
+
   self.view.backgroundColor = [UIColor redColor];
+
+  UIImageView *victoryView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"victory.jpg"]];
+  victoryView.frame = CGRectMake(20, 20, 100, 114);
+  [self.scrollView addSubview:victoryView];
+
+  UILabel *nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 140, 200, 40)];
+  nameLabel.text = @"Name: Steven Lu";
+  [self.scrollView addSubview:nameLabel];
+
+  UILabel *cityLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 200, 280, 40)];
+  cityLabel.text = @"From: New York";
+  [self.scrollView addSubview:cityLabel];
+
+  UITextView *biography = [[UITextView alloc] initWithFrame:CGRectMake(12, 260, 300, 180)];
+  biography.font = [UIFont fontWithName:@"Helvetica" size:15];
+  biography.editable = NO;
+  biography.text = @"Steven Lu is the greatest being ever";
+  [self.scrollView addSubview:biography];
+
+  UILabel *memberSinceLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 440, 280, 40)];
+  memberSinceLabel.text = @"September 2013";
+  [self.scrollView addSubview:memberSinceLabel];
+
+  [self.view addSubview:self.scrollView];
 	// Do any additional setup after loading the view.
 }
 
